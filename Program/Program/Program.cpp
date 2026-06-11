cf21b0d36b520fd212362750b90541a2beb82798
sukmin11 <sukmin1105@naver.com>
2026-06-11 19:43:33 +0900

    (2026.6.11) 단일 연결 리스트

---
+++ Program/Program/Program.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class List
+{
+private:
+    struct Node {
+        T data;
+        Node* next;
+
+    };
+
+    int size;
+
+    Node* head;
+
+public:
+
+    List()
+    {
+        size = 0;
+        head = nullptr;
+    }
+
+    void push_front(T data)
+    {
+        Node* newNode = new Node;
+
+        newNode->data = data;
+
+        if (head == nullptr)
+        {
+            head = newNode;
+            newNode->next = nullptr;
+        }
+        else
+        {
+            newNode->next = head;
+
+            head = newNode;
+        }
+
+        size++;
+
+    }
+    void pop_front()
+    {
+        if (head == nullptr)
+        {
+            cout << "linked list is empty" << endl;
+        }
+        else
+        {
+            Node* deleteNode = head;
+
+            head = deleteNode->next;
+
+            delete deleteNode;
+
+            size--;
+        }
+    }
+    void push_back(T data)
+    {
+
+        Node* newNode = new Node;
+
+        Node* currentNode = newNode;
+
+        newNode->data = data;
+
+        newNode->next = nullptr;
+
+
+        if (currentNode.next == nullptr)
+        {
+
+        }
+
+    }
+
+};
+
+int main()
+{
+    List<int> list;
+
+    list.push_front(10);
+
+    list.push_front(5);
+
+    list.pop_front();
+
+    list.pop_front();
+
+    list.pop_front();
+
+    return 0;
+}
+
