c2e34c26e6ba14c8278cd30d8ee7342432f75407
sukmin11 <sukmin1105@naver.com>
2026-06-12 19:45:07 +0900

    (2026.6.12) 이중 연결 리스트

--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -2,100 +2,50 @@
 
 using namespace std;
 
-template <typename T>
+template<typename T>
 class List
 {
 private:
     struct Node {
         T data;
         Node* next;
-
+        Node* previous;
     };
 
     int size;
-
     Node* head;
+    Node* tail;
 
 public:
-
     List()
     {
         size = 0;
         head = nullptr;
+        tail = nullptr;
     }
-
     void push_front(T data)
     {
         Node* newNode = new Node;
-
         newNode->data = data;
-
-        if (head == nullptr)
+        if (size == 0)
         {
             head = newNode;
-            newNode->next = nullptr;
+            tail = newNode;
         }
         else
         {
+            head->previous = newNode;
             newNode->next = head;
-
             head = newNode;
         }
-
         size++;
-
     }
-    void pop_front()
-    {
-        if (head == nullptr)
-        {
-            cout << "linked list is empty" << endl;
-        }
-        else
-        {
-            Node* deleteNode = head;
-
-            head = deleteNode->next;
-
-            delete deleteNode;
-
-            size--;
-        }
-    }
-    void push_back(T data)
-    {
-
-        Node* newNode = new Node;
-
-        Node* currentNode = newNode;
-
-        newNode->data = data;
-
-        newNode->next = nullptr;
-
-
-        if (currentNode.next == nullptr)
-        {
-
-        }
-
-    }
-
 };
 
 int main()
 {
     List<int> list;
-
     list.push_front(10);
-
-    list.push_front(5);
-
-    list.pop_front();
-
-    list.pop_front();
-
-    list.pop_front();
 
     return 0;
 }
