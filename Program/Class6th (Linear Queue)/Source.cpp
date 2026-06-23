fe623c95976fb43938a11aa57494353e89a461eb
sukmin11 <sukmin1105@naver.com>
2026-06-23 19:44:54 +0900

    (2026.6.23) 우선순위 큐

---
+++ Program/Class6th (Linear Queue)/Source.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#define SIZE 5
+
+using namespace std;
+
+template<typename T>
+class Queue
+{
+private:
+    T container[SIZE];
+    int m_front, m_rear;
+
+public:
+    Queue()
+    {
+        m_front = 0, m_rear = 0;
+        for (int i = 0; i < SIZE; i++)
+        {
+            container[i] = NULL;
+        }
+    }
+
+    void push(T data)
+    {
+        if (m_rear >= SIZE)
+        {
+            cout << "linear queue overflow" << endl;
+        }
+        else
+        {
+            container[m_rear++] = data;
+        }
+    }
+
+    void pop()
+    {
+        if (m_front == m_rear)
+        {
+            cout << "linear queue is empty" << endl;
+        }
+        else
+        {
+            container[m_front++] = NULL;
+        }
+    }
+
+    const bool& empty()
+    {
+        return m_rear == m_front;
+    }
+
+    const T& front()
+    {
+        return container[m_front];
+    }
+
+    const int& size()
+    {
+        return m_rear - m_front;
+    }
+};
+
+int main()
+{
+    Queue<int> queue;
+    queue.push(10);
+    queue.push(20);
+    queue.push(30);
+    queue.push(40);
+    queue.push(50);
+
+    while (queue.empty() == false)
+    {
+        cout << queue.front() << endl;
+        queue.pop();
+    }
+
+
+    return 0;
+}
+
--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -1,105 +1,30 @@
 ﻿#include <iostream>
+#define SIZE 4
 
 using namespace std;
 
 template<typename T>
-class Stack
+class Queue
 {
 private:
-    int capacity;
-    int highset;
-
-    T* container;
-
+    T container[SIZE];
+    
+    int m_rear, m_front;
 public:
-
-    Stack()
+    Queue()
     {
-        capacity = 0;
-        highset = -1;
-        container = nullptr;
-    }
-
-    void resize(int newSize)
-    {
-        capacity = newSize;
-        T* temporary = new T[capacity];
-        for (int i = 0; i < capacity; i++)
+        m_rear = SIZE - 1, m_front = SIZE - 1;
+        for (int i = 0; i < SIZE; i++)
         {
-            temporary[i] = NULL;
-        }
-        for (int i = 0; i < highset + 1; i++)
-        {
-            temporary[i] = container[i];
-        }
-        delete[] container;
-        container = temporary;
-    }
-
-    void push(T data)
-    {
-        if (capacity <= 0)
-        {
-            resize(1);
-        }
-        else if (highset + 1 >= capacity)
-        {
-            resize(capacity * 2);
-        }
-
-        container[++highset] = data;
-    }
-
-    void pop()
-    {
-        if (highset <= -1)
-        {
-            cout << "Stack is empty" << endl;
-        }
-        else
-        {
-            container[highset--] = NULL;
-        }
-    }
-
-    const bool& empty()
-    {
-        return highset == -1;
-    }
-
-    const int& size()
-    {
-        return highset + 1;
-    }
-
-    const T& top()
-    {
-        return container[highset];
-    }
-
-    ~Stack()
-    {
-        while (highset != -1)
-        {
-            pop();
+            container[i] = NULL;
         }
     }
 };
 
 int main()
 {
-    Stack<int> stack;
-    stack.push(10);
-    stack.push(20);
-    stack.push(30);
-    stack.push(40);
-    stack.push(50);
+    Queue<int> queue;
 
-    while (stack.empty() != 1)
-    {
-        cout << stack.top() << endl;
-        stack.pop();
-    }
 
     return 0;
 }
