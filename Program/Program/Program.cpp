90c404239e4048dd13a96aa84125c7b87bec4933
sukmin11 <sukmin1105@naver.com>
2026-06-25 19:43:43 +0900

    (2026.6.25) 해시 테이블

--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -36,13 +36,117 @@ public:
 
         container = temporary;
     }
+
+    void push(T data)
+    {
+        if (capacity <= 0)
+        {
+            resize(1);
+        }
+        else if(index >= capacity)
+        {
+            resize(capacity * 2);
+        }
+
+        container[index++] = data;
+
+        int child = index - 1;
+        int parent = (child - 1) / 2;
+
+        while (child > 0)
+        {
+            if (container[parent] < container[child])
+            {
+                std::swap(container[parent], container[child]);
+            }
+
+            child = parent;
+
+            parent = (child - 1) / 2;
+
+        }
+
+    }
+
+    void pop()
+    {
+        if (index <= 0)
+        {
+            cout << "priority queue is empty" << endl;
+        }
+        else
+        {
+            container[0] = container[--index];
+
+            container[index] = NULL;
+
+            int parent = 0;
+            int child = parent * 2 + 1;
+
+            while (child < index)
+            {
+                child = parent * 2 + 1;
+   
+                if (container[parent] < container[child + 1])
+                {
+                    child++;
+                }
+
+                if (container[child] < container[parent])
+                {
+                    break;
+                }
+                else
+                {
+                    std::swap(container[child], container[parent]);
+
+                    parent = child;
+                }
+
+
+            }
+        }
+    }
+
+    const bool& empty()
+    {
+        return index <= 0;
+    }
+
+    const int& size()
+    {
+        return index - 1;
+    }
+
+    const T& top()
+    {
+        return container[0];
+    }
+
+    ~PriorityQueue()
+    {
+        while (index > 0)
+        {
+            pop();
+        }
+    }
+
 };
 
 int main()
 {
     PriorityQueue<int> priorityQueue;
 
-    priorityQueue.resize(2);
+    priorityQueue.push(10);
+    priorityQueue.push(20);
+    priorityQueue.push(25);
+    priorityQueue.push(30);
+    priorityQueue.pop();
+    priorityQueue.pop();
+    priorityQueue.pop();
+    priorityQueue.pop();
+    priorityQueue.pop();
+
 
     return 0;
 }
