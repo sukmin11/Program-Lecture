13bbfdb857f025a38e97de1990cac6560352b185
sukmin11 <sukmin1105@naver.com>
2026-06-26 19:44:18 +0900

    (2026.6.26) 그래프

---
+++ Program/Class8th (Priority Queue)/Source.cpp
@@ -0,0 +1,153 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class PriorityQueue
+{
+private:
+    T* container;
+    int capacity;
+    int index;
+public:
+    PriorityQueue()
+    {
+        container = nullptr;
+        capacity = 0;
+        index = 0;
+    }
+
+    void resize(int newSize)
+    {
+        capacity = newSize;
+
+        T* temporary = new T[capacity];
+
+        for (int i = 0; i < capacity; i++)
+        {
+            temporary[i] = NULL;
+        }
+        for (int i = 0; i < index; i++)
+        {
+            temporary[i] = container[i];
+        }
+
+        delete[] container;
+
+        container = temporary;
+    }
+
+    void push(T data)
+    {
+        if (capacity <= 0)
+        {
+            resize(1);
+        }
+        else if (index >= capacity)
+        {
+            resize(capacity * 2);
+        }
+
+        container[index++] = data;
+
+        int child = index - 1;
+        int parent = (child - 1) / 2;
+
+        while (child > 0)
+        {
+            if (container[parent] < container[child])
+            {
+                std::swap(container[parent], container[child]);
+            }
+
+            child = parent;
+
+            parent = (child - 1) / 2;
+
+        }
+
+    }
+
+    void pop()
+    {
+        if (index <= 0)
+        {
+            cout << "priority queue is empty" << endl;
+        }
+        else
+        {
+            container[0] = container[--index];
+
+            container[index] = NULL;
+
+            int parent = 0;
+            int child = parent * 2 + 1;
+
+            while (child < index)
+            {
+                child = parent * 2 + 1;
+
+                if (container[child] < container[child + 1])
+                {
+                    child++;
+                }
+
+                if (container[child] < container[parent])
+                {
+                    break;
+                }
+                else
+                {
+                    std::swap(container[child], container[parent]);
+
+                    parent = child;
+                }
+
+
+            }
+        }
+    }
+
+    const bool& empty()
+    {
+        return index <= 0;
+    }
+
+    const int& size()
+    {
+        return index;
+    }
+
+    const T& top()
+    {
+        return container[0];
+    }
+
+    ~PriorityQueue()
+    {
+        delete[] container;
+    }
+
+};
+
+int main()
+{
+    PriorityQueue<int> priorityQueue;
+
+    priorityQueue.push(10);
+    priorityQueue.push(20);
+    priorityQueue.push(25);
+    priorityQueue.push(30);
+    priorityQueue.push(40);
+
+    while (priorityQueue.empty() == false)
+    {
+        cout << priorityQueue.top() << endl;
+        priorityQueue.pop();
+    }
+
+    cout << "priority queue is size : " << priorityQueue.size() << endl;
+
+    return 0;
+}
+
--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -2,151 +2,67 @@
 
 using namespace std;
 
-template<typename T>
-class PriorityQueue
+template<typename KEY, typename VALUE>
+class HashTable
 {
 private:
-    T* container;
+    struct Node
+    {
+        KEY key;
+        VALUE value;
+        Node* next;
+    };
+
+    struct Bucket
+    {
+        int count;
+        Node* head;
+    };
+
+    int size;
     int capacity;
-    int index;
+
+    Bucket* bucket;
+
 public:
-    PriorityQueue()
+    HashTable()
     {
-        container = nullptr;
-        capacity = 0;
-        index = 0;
-    }
-
-    void resize(int newSize)
-    {
-        capacity = newSize;
-
-        T* temporary = new T[capacity];
-
+        size = 0;
+        capacity = 8;
+        bucket = new Bucket[capacity];
+        
         for (int i = 0; i < capacity; i++)
         {
-            temporary[i] = NULL;
-        }
-        for (int i = 0; i < index; i++)
-        {
-            temporary[i] = container[i];
-        }
-
-        delete[] container;
-
-        container = temporary;
-    }
-
-    void push(T data)
-    {
-        if (capacity <= 0)
-        {
-            resize(1);
-        }
-        else if(index >= capacity)
-        {
-            resize(capacity * 2);
-        }
-
-        container[index++] = data;
-
-        int child = index - 1;
-        int parent = (child - 1) / 2;
-
-        while (child > 0)
-        {
-            if (container[parent] < container[child])
-            {
-                std::swap(container[parent], container[child]);
-            }
-
-            child = parent;
-
-            parent = (child - 1) / 2;
-
-        }
-
-    }
-
-    void pop()
-    {
-        if (index <= 0)
-        {
-            cout << "priority queue is empty" << endl;
-        }
-        else
-        {
-            container[0] = container[--index];
-
-            container[index] = NULL;
-
-            int parent = 0;
-            int child = parent * 2 + 1;
-
-            while (child < index)
-            {
-                child = parent * 2 + 1;
-   
-                if (container[parent] < container[child + 1])
-                {
-                    child++;
-                }
-
-                if (container[child] < container[parent])
-                {
-                    break;
-                }
-                else
-                {
-                    std::swap(container[child], container[parent]);
-
-                    parent = child;
-                }
-
-
-            }
+            bucket[i].head = nullptr;
+            bucket[i].count = 0;
         }
     }
 
-    const bool& empty()
+    template<typename KEY>
+    unsigned int hash_function(KEY key)
     {
-        return index <= 0;
+        return (unsigned int)key % capacity;
     }
 
-    const int& size()
+    template<>
+    unsigned int hash_function(const char* key)
     {
-        return index - 1;
-    }
-
-    const T& top()
-    {
-        return container[0];
-    }
-
-    ~PriorityQueue()
-    {
-        while (index > 0)
+        int sum = 0;
+        while (*key != NULL)
         {
-            pop();
+            sum = (unsigned int)*(key++);
         }
+        return sum % capacity;
     }
 
 };
 
 int main()
 {
-    PriorityQueue<int> priorityQueue;
-
-    priorityQueue.push(10);
-    priorityQueue.push(20);
-    priorityQueue.push(25);
-    priorityQueue.push(30);
-    priorityQueue.pop();
-    priorityQueue.pop();
-    priorityQueue.pop();
-    priorityQueue.pop();
-    priorityQueue.pop();
-
+    HashTable<const char *, int> hashTable;
+    cout << hashTable.hash_function("League of Legend") << endl;
+    cout << hashTable.hash_function("Yasuo") << endl;
+    cout << hashTable.hash_function("Janna") << endl;
 
     return 0;
 }
