17bde201743680c7da7a294680309b15921e6c45
sukmin11 <sukmin1105@naver.com>
2026-07-07 19:43:14 +0900

    (2026.7.7) 레드 블랙 트리

---
+++ Program/Class11th (Adjacency Matrix)/Source.cpp
@@ -0,0 +1,187 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class Graph
+{
+private:
+    int count; // 인접 행렬의 크기
+    int size; // 정점의 개수
+    int capacity; // 최대 용량
+
+    T* vertex; // 정점의 집합
+    int** matrix; // 인접 행렬
+public:
+    Graph()
+    {
+        count = 0;
+        size = 0;
+        capacity = 0;
+
+        vertex = nullptr;
+        matrix = nullptr;
+    }
+
+    void resize()
+    {
+        int** newMatrix = new int* [size];
+
+        for (int i = 0; i < size; i++)
+        {
+            newMatrix[i] = new int[size] {0};
+
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+            for (int j = 0; j < count; j++)
+            {
+                newMatrix[i][j] = matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+            delete[] matrix[i];
+        }
+
+        delete[] matrix;
+
+        matrix = newMatrix;
+
+        count = size;
+    }
+
+    void resize(int newSize)
+    {
+        capacity = newSize;
+
+        T* container = new T[capacity];
+
+        for (int i = 0; i < capacity; i++)
+        {
+            container[i] = NULL;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            container[i] = vertex[i];
+        }
+
+        delete[] vertex;
+
+        vertex = container;
+    }
+
+    void push(T data)
+    {
+        if (capacity <= 0)
+        {
+            resize(1);
+        }
+        else if (capacity <= size)
+        {
+            resize(capacity * 2);
+        }
+
+        vertex[size++] = data;
+    }
+
+    void edge(int i, int j)
+    {
+        if (size <= 0)
+        {
+            cout << "adjacency matrix is empty" << endl;
+        }
+        else if (i >= size || j >= size)
+        {
+            cout << "index out of range" << endl;
+        }
+        else
+        {
+            if (matrix == nullptr)
+            {
+                count = size;
+
+                matrix = new int* [size];
+
+                for (int i = 0; i < size; i++)
+                {
+                    matrix[i] = new int[size];
+
+                    for (int j = 0; j < size; j++)
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+            else if (count < size)
+            {
+                resize();
+            }
+
+            matrix[i][j] = 1;
+            matrix[j][i] = 1;
+        }
+
+    }
+
+    void render()
+    {
+        cout << "  ";
+
+        for (int i = 0; i < count; i++)
+        {
+            cout << vertex[i] << " ";
+        }
+
+        cout << endl;
+
+        for (int i = 0; i < count; i++)
+        {
+            cout << vertex[i] << " ";
+
+            for (int j = 0; j < count; j++)
+            {
+                cout << matrix[i][j] << " ";
+            }
+
+            cout << endl;
+        }
+    }
+
+    ~Graph()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            delete[] matrix[i];
+        }
+
+        delete[] matrix;
+
+        delete[] vertex;
+    }
+};
+
+int main()
+{
+    Graph<char> graph;
+
+    graph.push('A');
+    graph.push('B');
+    graph.push('C');
+    graph.push('D');
+
+    graph.edge(0, 1);
+    graph.edge(0, 2);
+    graph.edge(1, 3);
+    graph.edge(2, 3);
+
+    graph.push('E');
+    graph.edge(3, 4);
+
+    graph.render();
+
+    return 0;
+}
